de88193dd2cb6b976cb0932788d278b11686e277
req50844 <skip.paschall@mutualofomaha.com>
2017-06-12 08:34:27 -0500

    Add styles to pages

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,22 +1,27 @@
 var React = require('react');
 
-// var About = React.createClass({
-//   render: function () {
-//     return (
-//       <h3>About Component</h3>
-//     )
-//   }
-// });
-
-// var About = function(props) {
-//   return (
-//     <h3>About Component</h3>
-//   )
-// };
-
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React. I have built this
+        for The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   )
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -10,7 +11,18 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples Component!</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
   )
 };
 
